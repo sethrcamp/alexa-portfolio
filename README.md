9388831bf04273679ac3f4483ae236dddef86f66
Seth Campbell <sethrcamp@gmail.com>
2018-10-21 14:38:00 -0400

    initial test

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# alexa-portfolio
-This is the alexa skill api for my personal portfolio.
+# hide-and-go-seek
+This is an api for an alexa skill for my personal portfolio!
---
+++ src/View/Test.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$app->group('/', function() use ($app) {
+
+    $app->post('', function ($request, $response, $args) {
+        $res = [
+            "version" => "1.0",
+            "response" => [
+                "outputSpeech" => [
+                    "type" => "PlainText",
+                    "text" => "Hi! This worked!",
+                    "playBehavior" => "REPLACE_ALL"
+                ]
+            ]
+        ];
+
+        return $response->withJson($res);
+    });
+
+
+});
