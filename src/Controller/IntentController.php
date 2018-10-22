a1ae1168c4aa17e6de234dc14d254d3e756d0ef6
Seth Campbell <sethrcamp@gmail.com>
2018-10-21 20:58:23 -0400

    work intent

--- src/Controller/IntentController.php
+++ src/Controller/IntentController.php
@@ -44,7 +44,13 @@ class IntentController {
     }
 
     public static function work($request, $response, $args) {
-        $message = "This intent is not yet implemented";
+        $message = "Seth is currently working as a Development Master and Junior Project Manager at the Digital Corps, ".
+                   "a student run digital production group lead by a few Ball State Staff members. Over his time at the ".
+                   "Digital Corps, Seth has been promoted twice, starting as an Apprentice, then working his way to a ".
+                   "Specialist before becoming a Master. He has been with the Digital Corps since September of 2016, and ".
+                   "since then, he has been privileged to work on over 20 projects including work in frontend, backend, ".
+                   "mobile, AR, and VR development. He also has had project management experience, once managing a project ".
+                   "that included over 25 of his peers. To learn more about the projects Seth has worked on, just ask!";
 
         $alexa_response = self::get_response_object($message);
         return $response->withJson($alexa_response);
