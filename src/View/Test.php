4ec7fa3c4214234b17ed2fb59b9d61f44782012e
Seth Campbell <sethrcamp@gmail.com>
2018-10-25 16:22:10 -0400

    this?

--- src/View/Test.php
+++ src/View/Test.php
@@ -16,47 +16,54 @@ $app->group('/', function() use ($app) {
             die(DEV_MODE ? "Unvalidated" : "");
         }
 
-        if($body['request']['type'] === "SessionEndedRequest") {
-            return $response;
-        }
+        if($body['request']['type'] !== "SessionEndedRequest") {
+            
+            if ($body['request']['type'] === "LaunchRequest") {
+                return IntentController::start($request, $response, $args);
+            }
 
-        if($body['request']['type'] === "LaunchRequest") {
-            return IntentController::start($request, $response, $args);
-        }
+            $intent_name = $body['request']['intent']['name'];
 
-        $intent_name = $body['request']['intent']['name'];
+            switch ($intent_name) {
+                case "about":
+                    return IntentController::about($request, $response, $args);
+                case "project":
+                    return IntentController::project($request, $response, $args);
+                case "work":
+                    return IntentController::work($request, $response, $args);
+                case "more":
+                    return IntentController::more($request, $response, $args);
+                case "AMAZON.HelpIntent":
+                    return IntentController::more($request, $response, $args);
+                case "AMAZON.CancelIntent":
+                    return IntentController::stop($request, $response, $args);
+                case "AMAZON.StopIntent":
+                    return IntentController::stop($request, $response, $args);
+                default:
+                    {
 
-        switch($intent_name) {
-            case "about":                  return IntentController::about($request, $response, $args);
-            case "project":                return IntentController::project($request, $response, $args);
-            case "work":                   return IntentController::work($request, $response, $args);
-            case "more":                   return IntentController::more($request, $response, $args);
-            case "AMAZON.HelpIntent":      return IntentController::more($request, $response, $args);
-            case "AMAZON.CancelIntent":    return IntentController::stop($request, $response, $args);
-            case "AMAZON.StopIntent":      return IntentController::stop($request, $response, $args);
-            default: {
+                        if ($intent_name === "AMAZON.FallbackIntent") {
+                            $error_message = "Hmm, I didn't quite understand that. Try asking what Seth is like, about his projects, about his work history, or about where to learn more information.";
+                            $should_end_session = false;
+                        } else {
+                            $error_message = "Unfortunately, I seem to be having problems. Check back later!";
+                            $should_end_session = true;
+                        }
 
-                if($intent_name === "AMAZON.FallbackIntent") {
-                    $error_message = "Hmm, I didn't quite understand that. Try asking what Seth is like, about his projects, about his work history, or about where to learn more information.";
-                    $should_end_session = false;
-                } else {
-                    $error_message = "Unfortunately, I seem to be having problems. Check back later!";
-                    $should_end_session = true;
-                }
+                        $res = [
+                            "version" => "1.0",
+                            "response" => [
+                                "outputSpeech" => [
+                                    "type" => "PlainText",
+                                    "text" => $error_message,
+                                    "playBehavior" => "REPLACE_ALL"
+                                ],
+                                "shouldEndSession" => $should_end_session
+                            ]
+                        ];
 
-                $res = [
-                    "version" => "1.0",
-                    "response" => [
-                        "outputSpeech" => [
-                            "type" => "PlainText",
-                            "text" => $error_message,
-                            "playBehavior" => "REPLACE_ALL"
-                        ],
-                        "shouldEndSession" => $should_end_session
-                    ]
-                ];
-
-                return $response->withJson($res);
+                        return $response->withJson($res);
+                    }
             }
         }
     });
