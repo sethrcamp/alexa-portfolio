ab20bd61942d33c57a786fc47acc7d5388dbb4a1
Seth Campbell <sethrcamp@gmail.com>
2018-10-25 20:34:14 -0400

    debug

--- src/valid_request.php
+++ src/valid_request.php
@@ -66,7 +66,7 @@ $ECHO_CERT_CACHE = '/var/cache/amazon_echo/';
     $valid_cert = valid_cert( $jsonRequest, $data, $ECHO_CERT_CACHE );
     if ( $valid_cert != 1 ) {
         return array ( 'success' => 5,
-                       'message' => $valid_cert );
+                       'message' => $valid_cert." ".$data );
     }
 
     // Validate time stamp
